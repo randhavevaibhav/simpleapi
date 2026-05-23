8865fb67cc7617d742c1b1cc8812a5746c1ade1e
randhavevaibhav <randhavevaibhav3@gmail.com>
2026-05-23 05:38:36 -0700

    added bulld

---
+++ dist/packages/db/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = () => console.log("say hi");
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
---
+++ dist/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { hello } from "db";
+dotenv.config();
+const app = express();
+app.get("/", (_, res) => {
+    res.send("Server Started !!");
+    hello();
+});
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+        console.log(`Server started on PORT ${PORT}`);
+    });
+}
+export default app;
+//# sourceMappingURL=server.js.map
